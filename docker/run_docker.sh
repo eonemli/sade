c4c9c80229152476a92504559252980bcd291684
ahsanMah <mahmood.ahsan@outlook.com>
2024-01-05 17:25:15 +0000

    Updated docker run

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+NAME="sade"
+PORT=9990
+CODESPACE=/GROND_STOR/amahmood/workspace/
+#CODESPACE=/ASD/ahsan_projects/test/
 
-NAME="test"
-PORT = 9999
-# CODESPACE=/GROND_STOR/amahmood/workspace/
-CODESPACE=/ASD/ahsan_projects/test/
-
-# docker stop $NAME-docker || true # Exits gracefully if container doesnt exist
+docker stop $NAME-docker || true # Exits gracefully if container doesnt exist
 
 docker run \
 	-d \
@@ -22,5 +21,6 @@ docker run \
 	--mount type=bind,src="/UTexas",target=/UTexas \
 	--mount type=bind,src=$CODESPACE,target=/codespace \
 	-p $PORT:8888 \
+	-p 6006:6006 \
 	$USER/pytorch_sde:latest \
 	jupyter lab --ip 0.0.0.0 --notebook-dir=/ --no-browser --allow-root
