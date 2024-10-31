9bb1a8ed73ca0503f890d14592150ae6d83fb4d6
ahsanMah <mahmood.ahsan@outlook.com>
2024-10-31 12:25:10 -0400

    Added flag to not restore optimizer

--- sade/run/utils.py
+++ sade/run/utils.py
@@ -8,46 +8,58 @@ import torch
 from sade.models.ema import ExponentialMovingAverage
 
 
-def restore_checkpoint(ckpt_dir, state, device, raise_error=False):
+def restore_checkpoint(
+    ckpt_dir, state, device, restore_optim_states=True, raise_error=False
+):
     if not os.path.exists(ckpt_dir):
         if raise_error:
             raise FileNotFoundError(f"No checkpoint found at {ckpt_dir}")
         else:
             logging.warning(
-                f"No checkpoint found at {ckpt_dir}. " f"Returned the same state as input"
+                f"No checkpoint found at {ckpt_dir}. "
+                f"Returned the same state as input"
             )
         return state
     else:
         loaded_state = torch.load(ckpt_dir, map_location=device)
-        if state.get("optimizer") is not None:
-            state["optimizer"].load_state_dict(loaded_state["optimizer"])
-            state["optimizer"].param_groups[0]["capturable"] = True
-
         state["model"].load_state_dict(loaded_state["model"], strict=False)
         state["ema"].load_state_dict(loaded_state["ema"])
         state["step"] = loaded_state["step"]
 
-        if "scheduler" in loaded_state and state["scheduler"] is not None:
-            state["scheduler"].load_state_dict(loaded_state["scheduler"])
+        if restore_optim_states:
+            if state.get("optimizer") is not None:
+                state["optimizer"].load_state_dict(loaded_state["optimizer"])
+                state["optimizer"].param_groups[0]["capturable"] = True
 
-        if "grad_scaler" in loaded_state and "grad_scaler" in state:
-            state["grad_scaler"].load_state_dict(loaded_state["grad_scaler"])
+            if "scheduler" in loaded_state and state["scheduler"] is not None:
+                state["scheduler"].load_state_dict(loaded_state["scheduler"])
+
+            if "grad_scaler" in loaded_state and "grad_scaler" in state:
+                state["grad_scaler"].load_state_dict(loaded_state["grad_scaler"])
 
         logging.info(f"Loaded model state at step {state['step']} from {ckpt_dir}")
         return state
 
 
 def restore_pretrained_weights(ckpt_dir, state, device):
-    assert state["step"] == 0, "Can only load pretrained weights when starting a new run"
-    assert os.path.exists(ckpt_dir), f"Pretrain weights directory {ckpt_dir} does not exist"
-    assert state['model'].training, "Model must be in training mode to appropriately load pretrained weights"
+    assert (
+        state["step"] == 0
+    ), "Can only load pretrained weights when starting a new run"
+    assert os.path.exists(
+        ckpt_dir
+    ), f"Pretrain weights directory {ckpt_dir} does not exist"
+    assert (
+        state["model"].training
+    ), "Model must be in training mode to appropriately load pretrained weights"
 
     loaded_state = torch.load(ckpt_dir, map_location=device)
     state["model_checkpoint_step"] = loaded_state["step"]
     dummy_ema = ExponentialMovingAverage(state["model"].parameters(), decay=0.999)
     dummy_ema.load_state_dict(loaded_state["ema"])
     dummy_ema.lazy_copy_to(state["model"].parameters())
-    logging.info(f"Loaded pretrained EMA weights from {ckpt_dir} at {loaded_state['step']}")
+    logging.info(
+        f"Loaded pretrained EMA weights from {ckpt_dir} at {loaded_state['step']}"
+    )
 
     return state
 
@@ -113,6 +125,8 @@ def get_flow_rundir(config, workdir):
     hparams += (
         f"-nb{config.flow.num_blocks}-lr{config.flow.lr}-bs{config.training.batch_size}"
     )
-    hparams += f"-np{config.flow.patches_per_train_step}-kimg{config.flow.training_kimg}"
+    hparams += (
+        f"-np{config.flow.patches_per_train_step}-kimg{config.flow.training_kimg}"
+    )
     rundir = os.path.join(workdir, "flow", hparams)
     return rundir
