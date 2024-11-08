c99248b9b2eb9bdcb163ccee7f6f0bdb20f89f0a
ahsanMah <mahmood.ahsan@outlook.com>
2024-11-08 13:29:31 -0500

    updated matryoshka config

--- sade/configs/flows/matryoshka_gmm_flow_config.py
+++ sade/configs/flows/matryoshka_gmm_flow_config.py
@@ -7,13 +7,13 @@ def get_config():
     config = get_default_config()
 
     training = config.training
-    training.batch_size = 4
+    training.batch_size = 2
     training.log_freq = 5
-    training.use_fp16 = False
+    training.use_fp16 = True
     training.pretrained_checkpoint = "/ASD/ahsan_projects/braintypicality/workdir/hres/f2-1e4/checkpoints/checkpoint_115.pth"
 
     eval = config.eval
-    eval.batch_size = 32
+    eval.batch_size = 2
     experiment = eval.experiment
     experiment.train = "abcd-val"
     experiment.inlier = "abcd-test"
@@ -28,14 +28,14 @@ def get_config():
     flow.global_embedding_size = 512
     flow.input_norm = False
 
-    flow.patch_batch_size = 32
-    flow.patches_per_train_step = 2048
+    flow.patch_batch_size = 2048
+    flow.patches_per_train_step = 1<<14
     flow.training_kimg = 200
 
     # Config for patch sizes
     flow.local_patch_config = ml_collections.ConfigDict()
-    flow.local_patch_config.kernel_size = 3
-    flow.local_patch_config.padding = 1
+    flow.local_patch_config.kernel_size = 5
+    flow.local_patch_config.padding = 2
     flow.local_patch_config.stride = 1
 
     # Config for larger receptive fields outputting gobal context
